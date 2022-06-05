860523761465ac0ef93724c322fdf4744db3cd62
Vivek <55319798+thevivekpande@users.noreply.github.com>
2022-06-05 18:58:30 +0530

    Day_01 sol Added

---
+++ Day_01_SetMatrixZero.java
@@ -0,0 +1,27 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+    public static void setZeros(int matrix[][]) {
+        // Write your code here..
+         int col0=1, row=matrix.length, col=matrix[0].length;
+        
+        for(int i=0;i<row;i++){
+           if(matrix[i][0]==0) col0=0;
+            for(int j=1;j<col;j++){
+                if(matrix[i][j] == 0){
+                    matrix[i][0] = matrix[0][j]= 0;
+                }
+            }
+        }
+        
+        for(int i=row-1;i>=0;i--){
+            for(int j=col-1;j>=1;j--){
+                if(matrix[i][0]==0 || matrix[0][j]==0){
+                    matrix[i][j] = 0;
+                }
+            }
+          if(col0==0) matrix[i][0]=0;   
+        }
+    }
+
+}

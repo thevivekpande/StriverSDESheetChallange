d2f0b35fded5d2cd3b141c421e5620f7675bbe7f
Vivek <55319798+thevivekpande@users.noreply.github.com>
2022-06-11 23:54:52 +0530

    Day_07 sol Added

---
+++ Solutions/Day_07_MiddleOfLinkedList.java
@@ -0,0 +1,33 @@
+import java.util.* ;
+import java.io.*; 
+/****************************************************************
+
+    Following is the class structure of the Node class:
+
+       class Node 
+        { 
+        public:
+            int data;
+            Node next;
+            Node(int data) 
+            {
+              this.data = data;
+              this.next = null;
+            }
+        };
+
+*****************************************************************/
+
+public class Solution {
+    public static Node findMiddle(Node head) 
+    {
+        // Write your code here
+        Node slow=head;
+        Node fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+}

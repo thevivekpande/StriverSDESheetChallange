8f6fda34dbf7d54f4c6d949b15e9d73aa0e79c08
Vivek <55319798+thevivekpande@users.noreply.github.com>
2022-06-16 14:04:45 +0530

    Day_12 sol Added

--- Solutions/Day_12_JobSequencingProblem.java
+++ Solutions/Day_12_JobSequencingProblem.java
@@ -6,21 +6,21 @@ public class Solution
         // Write your code here
         int n=jobs.length;
         Arrays.sort(jobs, (a,b)->(b[1]-a[1]));
-        int maxDeadline=jobs[0][0];
+        int maxDeadline=0;
         for(int i=0;i<n;i++)
-            maxDeadline=Math.max(jobs[i][0], maxDeadline);
-        int result[]=new int[maxDeadline+1];
-        Arrays.fill(result, -1);
-        int jobProfits=0;
+            maxDeadline=Math.max(maxDeadline, jobs[i][0]);
+        boolean[] isVisited=new boolean[maxDeadline+1];
+        Arrays.fill(isVisited, false);
+        int res=0;
         for(int i=0;i<n;i++){
             for(int j=jobs[i][0];j>0;j--){
-                if(result[j]==-1){
-                    result[j]=i;
-                    jobProfits+=jobs[i][1];
+                if(!isVisited[j]){
+                    isVisited[j]=true;
+                    res+=jobs[i][1];
                     break;
                 }
             }
         }
-        return jobProfits;
+        return res;
     }
 }

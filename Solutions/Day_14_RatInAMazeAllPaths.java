e1e8f4768761966f947adcc341ec50399c397535
Vivek <55319798+thevivekpande@users.noreply.github.com>
2022-06-18 18:38:10 +0530

    Day_14 sol Added

--- Solutions/Day_14_RatInAMazeAllPaths.java
+++ Solutions/Day_14_RatInAMazeAllPaths.java
@@ -1,6 +1,7 @@
 import java.util.*;
 public class Solution {   
-    public static void dfs(int[][] maze, int i, int j, int n, int[][] ans, ArrayList<ArrayList<Integer>> res){
+    public static  ArrayList<ArrayList<Integer>> res;
+    public static void dfs(int[][] maze, int i, int j, int n, int[][] ans){
         if(i<0 || j<0 || i>=n || j>=n || maze[i][j]==0 || ans[i][j]==1) return;
         
         if(i==n-1 && j==n-1 && maze[i][j]==1){
@@ -9,10 +10,10 @@ public class Solution {
         }
         
         ans[i][j]=1;
-        dfs(maze, i+1, j, n, ans, res);
-        dfs(maze, i-1, j, n, ans, res);
-        dfs(maze, i, j+1, n, ans, res);
-        dfs(maze, i, j-1, n, ans, res);
+        dfs(maze, i+1, j, n, ans);
+        dfs(maze, i-1, j, n, ans);
+        dfs(maze, i, j+1, n, ans);
+        dfs(maze, i, j-1, n, ans);
         ans[i][j]=0;
     }
     
@@ -27,8 +28,8 @@ public class Solution {
     public static ArrayList<ArrayList<Integer>> ratInAMaze(int[][] maze, int n) {
         // Write your code here.
         int[][] ans=new int[n][n];
-        ArrayList<ArrayList<Integer>> res=new ArrayList();
-        dfs(maze, 0, 0, n, ans, res);
+        res=new ArrayList();
+        dfs(maze, 0, 0, n, ans);
         return res;
         
     }

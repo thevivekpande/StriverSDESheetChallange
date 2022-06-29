20ac76352da5f4c7a6cea4d7fbf45c845498a4be
Vivek Pandey <vivek143.iimt@gmail.com>
2022-06-30 01:21:59 +0530

    Day_25 sol Added

--- Solutions/Day_25_LongestPalidromicSubstring.java
+++ Solutions/Day_25_LongestPalidromicSubstring.java
@@ -9,14 +9,15 @@ public class Solution {
     }
 	public static String longestPalinSubstring(String str) {
 		// Write your code here.
-        int start=0,last=0;
+        int start=0,last=0,currlen=0;
         for(int i=0;i<str.length();i++){
             int len1=expandFromMid(str, i, i);
             int len2=expandFromMid(str, i, i+1);
             int len=Math.max(len1, len2);
-            if(len>(last-start+1)){
+            if(len>currlen){
                 start=i-((len-1)/2);
                 last=i+((len)/2);
+                currlen=len;
             }
         }
         return str.substring(start,last+1);

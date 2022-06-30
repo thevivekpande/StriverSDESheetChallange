4d2d7025153a040caa1c3889f4b331a776c6b090
Vivek Pandey <vivek143.iimt@gmail.com>
2022-06-30 18:58:29 +0530

    Day_26 sol Added

---
+++ Solutions/Day_26_ImplementATOIFunction.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public static int atoi(String str) {
+        // Write your code here.
+        if(str.length()==0) return 0;
+        int i=0, sign=1, n=str.length();
+        while(i<n && str.charAt(i)==' ') i++;
+        if(i>=n) return 0;
+        if(str.charAt(i)=='+' || str.charAt(i)=='-') sign=str.charAt(i++)=='+'?1:-1;
+        long res=0;
+        while(i<n){
+            if(Character.isDigit(str.charAt(i)))
+                res=res*10+(str.charAt(i)-'0');
+            i++;
+        }
+        return (int)(sign*res);
+    }
+}

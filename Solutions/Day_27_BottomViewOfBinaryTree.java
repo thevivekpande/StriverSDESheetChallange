d322fcdb2365bd5fbc903c58dd9b1833630f2658
Vivek Pandey <vivek143.iimt@gmail.com>
2022-07-01 22:59:47 +0530

    Day_27 sol Added

---
+++ Solutions/Day_27_BottomViewOfBinaryTree.java
@@ -0,0 +1,51 @@
+/*********************************************
+
+    class BinaryTreeNode {
+        int val;
+        BinaryTreeNode left;
+        BinaryTreeNode right;
+
+        BinaryTreeNode(int val) {
+            this.val = val;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+*********************************************/
+
+import java.util.*;
+
+class Pair{
+    BinaryTreeNode node;
+    int hd;
+    Pair(BinaryTreeNode _node, int _hd){
+        node=_node;
+        hd=_hd;
+    }
+}
+
+public class Solution {
+    public static ArrayList<Integer> bottomView(BinaryTreeNode root) {
+         // Write your code here.
+        ArrayList<Integer> res= new ArrayList();
+        Map<Integer, Integer> map=new TreeMap();
+        if(root==null) return res;
+        Queue<Pair> q=new LinkedList();
+        q.offer(new Pair(root, 0));
+        map.put(0, root.val);
+        while(!q.isEmpty()){
+                Pair temp= q.poll();
+                map.put(temp.hd, temp.node.val);
+                if(temp.node.left!=null){
+                    q.offer(new Pair(temp.node.left, temp.hd-1));
+                }
+                if(temp.node.right!=null){
+                    q.offer(new Pair(temp.node.right, temp.hd+1));
+                }
+        }
+        for(Map.Entry<Integer, Integer> entry: map.entrySet())
+            res.add(entry.getValue());
+        return res;
+    }
+}

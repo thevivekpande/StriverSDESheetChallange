4ba444be56ad38de337cc62be4d346fea8e3560a
Vivek Pandey <vivek143.iimt@gmail.com>
2022-07-05 16:46:19 +0530

    Day_31 sol Added

--- Solutions/Day_31_PairSumBST.java
+++ Solutions/Day_31_PairSumBST.java
@@ -16,6 +16,7 @@
     }
 
 *************************************************************/
+// #M1 Using HashSet
 import java.util.*;
 public class Solution {
     static HashSet<Integer> set=new HashSet<>();
@@ -33,3 +34,23 @@ public class Solution {
 	}
 }
 
+// #M2 Using Binary Search
+public class Solution {
+    public static boolean binarySearch(BinaryTreeNode root, BinaryTreeNode cur, int k){
+        while(root!=null){
+            if(root.data==k && root!=cur) return true;
+            else if(root.data>k) root=root.left;
+            else root=root.right;
+        }
+        return false;
+    }
+    public static boolean dfs(BinaryTreeNode root, BinaryTreeNode cur, int k){
+        if(cur==null) return false;
+        return (binarySearch(root, cur, k-cur.data) || dfs(root, cur.left, k) || dfs(root, cur.right, k));
+    }
+	public static boolean pairSumBst(BinaryTreeNode root, int k) {
+		// Write your code here.
+        if(root==null) return false;
+        return dfs(root, root, k);
+	}
+}

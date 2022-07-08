ed7a2e7292ac0051f0b8c943744faf3db5077258
Vivek Pandey <vivek143.iimt@gmail.com>
2022-07-08 16:37:49 +0530

    Day_34 sol Added;

--- Solutions/Day_34_CycleDetectionOfUndirectedGraph.java
+++ Solutions/Day_34_CycleDetectionOfUndirectedGraph.java
@@ -1,3 +1,4 @@
+// Using BFS
 import java.util.*;
 
 class Pair{
@@ -41,6 +42,44 @@ public class Solution {
         for(int i=1;i<=n;i++)
             if(!isVis[i])
                 if(isCycle(adj, isVis, i))
+                    return "Yes";
+        return "No";
+    }
+    
+}
+
+// Using DFS
+
+import java.util.*;
+
+public class Solution {
+    private static boolean isCycle(int node, int par, HashMap<Integer, ArrayList<Integer>> adj, boolean[] isVis){
+        isVis[node]=true;
+        for(int it : adj.get(node)){
+            if(!isVis[it]){
+                isVis[it]=true;
+                if(isCycle(it, node, adj, isVis))
+                    return true;
+            }else if(par!=it)
+                return true; 
+        }
+        return false;
+    }
+    
+    public static String cycleDetection(int[][] edges, int n, int m) {
+        // Write your code here.
+        HashMap<Integer, ArrayList<Integer>> adj=new HashMap();
+        for(int i=1;i<=n;i++)
+            adj.put(i, new ArrayList());
+        for(int i=0;i<m;i++){
+            adj.get(edges[i][0]).add(edges[i][1]);
+            adj.get(edges[i][1]).add(edges[i][0]);
+        }
+        boolean[] isVis=new boolean[n+1];
+        
+        for(int i=1;i<=n;i++)
+            if(!isVis[i])
+                if(isCycle(i, -1, adj, isVis))
                     return "Yes";
         return "No";
     }

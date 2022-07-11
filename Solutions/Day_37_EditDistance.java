821b62b6fbb38957afbf335965a9d62cd82c79f5
Vivek Pandey <vivek143.iimt@gmail.com>
2022-07-11 20:29:40 +0530

    Day_37 sol Added

--- Solutions/Day_37_EditDistance.java
+++ Solutions/Day_37_EditDistance.java
@@ -38,3 +38,24 @@ public class Solution {
 
 
 // Tabulation (Bottom Up)
+import java.util.*;
+public class Solution {
+    public static int editDistance(String str1, String str2) {
+        //Your code goes here
+        int n=str1.length(), m=str2.length();
+        int[][] dp=new int[n+1][m+1];
+        for(int i=0;i<=m;i++)
+            dp[0][i]=i;
+        for(int i=0;i<=n;i++)
+            dp[i][0]=i;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(str1.charAt(i-1)==str2.charAt(j-1))
+                    dp[i][j]=dp[i-1][j-1];
+                else
+                    dp[i][j]=1+Math.min(dp[i][j-1], Math.min(dp[i-1][j], dp[i-1][j-1]));
+            }
+        }
+        return dp[n][m];
+    }
+}

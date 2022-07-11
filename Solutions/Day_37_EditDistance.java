a2fffab11fb7d159fddf4439cb6775fc5cf5d70b
Vivek Pandey <vivek143.iimt@gmail.com>
2022-07-11 20:23:49 +0530

    Day_37 sol Added

---
+++ Solutions/Day_37_EditDistance.java
@@ -0,0 +1,40 @@
+// Recursion (TLE)
+public class Solution {
+    public static int helper(int n, String str1, int m, String str2){
+        if(n<0) return m+1;
+        if(m<0) return n+1;
+        if(str1.charAt(n)==str2.charAt(m))
+            return helper(n-1, str1, m-1, str2);
+        return 1+Math.min(helper(n-1, str1, m, str2), Math.min(helper(n, str1, m-1, str2), helper(n-1, str1, m-1, str2)));
+    }
+    public static int editDistance(String str1, String str2) {
+        //Your code goes here
+        int n=str1.length(), m=str2.length();
+        return helper(n-1, str1, m-1, str2);
+    }
+}
+
+
+// Memoization (Top - Down)
+import java.util.*;
+public class Solution {
+    public static int helper(int n, String str1, int m, String str2, int[][] dp){
+        if(n<0) return m+1;
+        if(m<0) return n+1;
+        if(dp[n][m]!=-1) return dp[n][m];
+        if(str1.charAt(n)==str2.charAt(m))
+            return dp[n][m]=helper(n-1, str1, m-1, str2, dp);
+        return dp[n][m]=1+Math.min(helper(n-1, str1, m, str2, dp), Math.min(helper(n, str1, m-1, str2, dp), helper(n-1, str1, m-1, str2, dp)));
+    }
+    public static int editDistance(String str1, String str2) {
+        //Your code goes here
+        int n=str1.length(), m=str2.length();
+        int[][] dp=new int[n+1][m+1];
+        for(int[] row:dp)
+            Arrays.fill(row, -1);
+        return helper(n-1, str1, m-1, str2, dp);
+    }
+}
+
+
+// Tabulation (Bottom Up)

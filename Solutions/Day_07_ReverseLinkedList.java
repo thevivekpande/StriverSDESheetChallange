393ec1d6f54858a4eeb3d903644e793e8e5c1f18
Vivek <55319798+thevivekpande@users.noreply.github.com>
2022-07-25 19:42:08 +0530

    Update Day_07_ReverseLinkedList.java
    Recursive code added

--- Solutions/Day_07_ReverseLinkedList.java
+++ Solutions/Day_07_ReverseLinkedList.java
@@ -32,3 +32,27 @@ public class Solution
         return prev;
     }
 }
+
+// Recursive Code
+/*
+    1->2->3->4->N
+    
+    Assuming that recursive call has already reversed the rest of the node and we have to reverse only initial node.
+    1->(2<-3<-4<-newHead)
+    head -> 1
+    head.next->2
+    head.next.next=head => 2->1
+    head.next=null => 2->1->N
+    Final => N<-1<-2<-3<-4<-newHead
+*/
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head==null || head.next==null)
+            return head;
+        ListNode node=reverseList(head.next);
+        head.next.next=head;
+        head.next=null;
+        return node;
+    }
+}

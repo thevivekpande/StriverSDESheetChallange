aeb76f35350f8c7ba424b53b9dfbfc95ee612163
Vivek <55319798+thevivekpande@users.noreply.github.com>
2022-07-27 12:13:11 +0530

    Update Day_29_FlattenBinaryTreeToLinkedList.java

--- Solutions/Day_29_FlattenBinaryTreeToLinkedList.java
+++ Solutions/Day_29_FlattenBinaryTreeToLinkedList.java
@@ -37,3 +37,22 @@ public class Solution
         return root;
     }
 }
+
+// Iterative Solution (Leetcode)
+class Solution {
+    public void flatten(TreeNode root) {
+        TreeNode curr=root;
+        TreeNode prev=null;
+        while(curr!=null){
+            if(curr.left!=null){
+                prev=curr.left;
+                while(prev.right!=null)
+                    prev=prev.right;
+                prev.right=curr.right;
+                curr.right=curr.left;
+                curr.left=null;
+            }
+            curr=curr.right;
+        }
+    }
+}
